3fe5b5f5ef2c450a02086287882c346a60f3b778
Ibon Garay Bengoa <igaraybengoa@gmail.com>
2020-02-05 11:42:45 +0100

    V P 1

--- bets/views.py
+++ bets/views.py
@@ -39,13 +39,13 @@ CAPITAL_INICIAL_TOTAL_APUESTAS = 10
 from celery.schedules import crontab
 from celery.task import periodic_task
 from datetime import timedelta
-@periodic_task(run_every=timedelta(seconds=2))
-def every_monday_morning():
-    print("This is run every Monday morning at 7:30")
+#@periodic_task(run_every=timedelta(seconds=2))
+#def every_monday_morning():
+#    print("This is run every Monday morning at 7:30")
 
-@periodic_task(run_every=crontab(hour=7, minute=30, day_of_week="mon"))
-def every_monday_morning():
-    print("This is run every Monday morning at 7:30")
+#@periodic_task(run_every=crontab(hour=7, minute=30, day_of_week="mon"))
+#def every_monday_morning():
+#    print("This is run every Monday morning at 7:30")
 
 @periodic_task(run_every=timedelta(seconds=2))
 def every_monday_morning():
