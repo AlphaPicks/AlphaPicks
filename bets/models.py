09d2da4bcc935d526ec8df47bab0fc5ec8898a18
Ibon Garay Bengoa <igaraybengoa@gmail.com>
2020-02-14 08:19:09 +0100

    Actualizar beneficios

--- bets/models.py
+++ bets/models.py
@@ -12,6 +12,19 @@ class Beneficios(models.Model):
     def __unicode__(self):
         return self.dia
 
+class BeneficiosMes(models.Model):
+    dia = models.DateField()
+    capital_inicial = models.DecimalField(max_digits = 15, decimal_places = 2) 
+    ganancias_brutas = models.DecimalField(max_digits = 15, decimal_places = 2) 
+    ganancias_netas = models.DecimalField(max_digits = 15, decimal_places = 2) 
+    porcentaje_beneficio = models.DecimalField(max_digits = 15, decimal_places = 2) 
+    porcentaje_beneficio_frente_al_inicial = models.DecimalField(max_digits = 15, decimal_places = 2) 
+    temporada = models.IntegerField(default = 0)
+    mes = models.IntegerField(default = 0)
+    objects = models.Manager()
+    def __unicode__(self):
+        return self.dia
+
 class Predicciones(models.Model):
     prediction = models.DecimalField(max_digits = 15, decimal_places = 2, default = 0) 
     date = models.DateField()
